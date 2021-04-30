ee1ead44bac7ca421130dfb2ea9a79c47ac6d715
fengzhao <fengzhao@powerlaw.ai>
2021-04-30 17:24:01 +0800

    路由分组

--- gee/gee.go
+++ gee/gee.go
@@ -1,6 +1,7 @@
 package gee
 
 import (
+	"log"
 	"net/http"
 )
 
@@ -8,22 +9,34 @@ import (
 type HandleFunc func(c *Context)
 
 type Engine struct {
+	*RouterGroup
 	router *router
+	groups []*RouterGroup
+}
+
+type RouterGroup struct {
+	prefix string
+	middlewares []HandleFunc
+	parent *RouterGroup
+	engine *Engine
 }
 
 // New is a constructor of gee.Engine
 func New() *Engine {
-	return &Engine{router: newRouter()}
+	engine := &Engine{router: newRouter()}
+	engine.RouterGroup = &RouterGroup{engine: engine}
+	engine.groups = []*RouterGroup{engine.RouterGroup}
+	return engine
 }
 
 // Get method
-func (e *Engine) Get(pattern string, handler HandleFunc) {
-	e.router.addRouter("GET", pattern, handler)
+func (group *RouterGroup) Get(pattern string, handler HandleFunc) {
+	group.addRouter("GET", pattern, handler)
 }
 
 // Post method
-func (e *Engine) Post(pattern string, handler HandleFunc) {
-	e.router.addRouter("POST", pattern, handler)
+func (group *RouterGroup) Post(pattern string, handler HandleFunc) {
+	group.addRouter("POST", pattern, handler)
 }
 
 // Run method
@@ -34,4 +47,23 @@ func (e *Engine) Run(addr string) (err error) {
 func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
 	c := newContext(w, r)
 	e.router.handle(c)
+}
+
+// create router group 
+func (group *RouterGroup) Group (prefix string) *RouterGroup {
+	engine := group.engine
+	newGroup := &RouterGroup{
+		prefix: group.prefix + prefix,
+		parent: group,
+		engine: engine,
+	}
+
+	engine.groups = append(engine.groups, newGroup)
+	return newGroup
+}
+
+func (group *RouterGroup) addRouter(method, comp string, handler HandleFunc)  {
+	pattern := group.prefix + comp
+	log.Printf("Route %4s - %s", method, pattern)
+	group.engine.router.addRouter(method, pattern, handler)		
 }
--- main.go
+++ main.go
@@ -9,24 +9,33 @@ import (
 func main() {
 	fmt.Println("hello world")
 	r := gee.New()
-	r.Get("/", func(c *gee.Context) {
+	r.Get("/index", func(c *gee.Context) {
 		c.Html(http.StatusOK, "<h1>Hello Gee<h1>")
 	})
-	r.Get("/hello", func(c *gee.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
-	})
-	r.Get("/hello/:name", func(c *gee.Context) {
-		c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
-	})
-	r.Get("/asset/*filepath", func(c *gee.Context) {
-		c.Json(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
-	})
-	r.Post("/login", func(c *gee.Context) {
-		c.Json(http.StatusOK, gee.H{
-			"username": c.PostForm("username"),
-			"password": c.PostForm("password"),
+	v1 := r.Group("/v1")
+	{
+		v1.Get("/index", func(c *gee.Context) {
+			c.Html(http.StatusOK, "<h1>Hello Gee<h1>")
 		})
-	})
+		v1.Get("/hello", func(c *gee.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Query("name"), c.Path)
+		})
+	}
+	v2 := r.Group("/v2")
+	{
+		v2.Get("/hello/:name", func(c *gee.Context) {
+			c.String(http.StatusOK, "hello %s, you're at %s\n", c.Param("name"), c.Path)
+		})
+		v2.Get("/asset/*filepath", func(c *gee.Context) {
+			c.Json(http.StatusOK, gee.H{"filepath": c.Param("filepath")})
+		})
+		v2.Post("/login", func(c *gee.Context) {
+			c.Json(http.StatusOK, gee.H{
+				"username": c.PostForm("username"),
+				"password": c.PostForm("password"),
+			})
+		})
+	}
 
 	err := r.Run(":9999")
 	if err != nil {
